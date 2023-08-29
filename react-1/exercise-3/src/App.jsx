d960c39ba24c78d641650a06eb63140dccfdf14e
NathanMun <nathawut.munsri@gmail.com>
2023-08-29 16:01:28 +0700

    react-1-exercise-3-4

--- react-1/exercise-3/src/App.jsx
+++ react-1/exercise-3/src/App.jsx
@@ -10,14 +10,32 @@ function App() {
 
 const RegistrationForm = () => {
   return (
-    // Code here
+    <div class="container">
+      <h1>Registration Form</h1>
+      <form id="registrationForm">
+        <div class="form-group">
+          <label for="name">Name:</label>
+          <input type="text" id="name" name="name" required />
+        </div>
+        <div class="form-group">
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 };
 
 const DisplayMember = () => {
   return (
-    // Code here
+    <div class="members">
+      <h2>Registered Members</h2>
+      <div class="member">John Doe (john@example.com)</div>
+      <div class="member">Jane Smith (jane@example.com)</div>
+    </div>
   );
 };
 
 export default App;
+
--- react-1/exercise-4/src/App.jsx
+++ react-1/exercise-4/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,43 @@ function App() {
       <TableDisplay />
     </div>
   );
+}
+
+const RunningForm = () => {
+  return ( 
+       <form id="data-form">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" /><br /><br />
+
+      <label for="age">Age:</label>
+      <input type="number" id="age" name="age" /><br /><br />
+
+      <label for="weight">Weight:</label>
+      <input type="number" id="weight" name="weight" /><br /><br />
+
+      <label for="running">Current Running (Minutes):</label>
+      <input type="number" id="running" name="running" /><br /><br />
+
+      <button type="button" onClick="addData()">Add Data</button>
+    </form>    
+  )
+}
+
+const TableDisplay = () => {
+  return (
+    <div>
+    <h1>Entered Data</h1>
+    <table id="data-table" border="1" width="100%">
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Weight</th>
+        <th>Current Running (Minutes)</th>
+        <th>Good Running</th>
+      </tr>
+    </table>
+    </div>
+  )
 }
 
 export default App;
